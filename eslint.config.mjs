562b1c4685c71ebc41552a33083d0d13f607f3d9
Deepak Barwal <deepakbarwal98@gmail.com>
2024-07-06 11:13:59 +0530

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    eslintConfigPrettier,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-    },
-    {
-        ignores: ['dist/*', 'eslint.config.mjs'],
-    },
-    {
-        rules: {},
-    },
-);
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    ignores: ['dist/*', 'eslint.config.mjs']
+  },
+  {
+    rules: {}
+  }
+)
